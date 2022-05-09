a9930a3daa3c3d419563a198c1b949e672c8d23f
waynewhying <wayne500000w@gmail.com>
2022-05-09 22:46:58 +0800

    some bugs are fixed

--- ML/predict/main.py
+++ ML/predict/main.py
@@ -1,8 +1,8 @@
 import numpy as np
 import tensorflow as tf
 
-images = np.load('./test_image/2.npy')
+images = np.load('/predict/test_image/2.npy')
 
-model = tf.keras.models.load_model('model')
+model = tf.keras.models.load_model('/predict/model')
 pred = model.predict(images.reshape(-1, 28, 28))
 print("The answer is", np.argmax(pred))
